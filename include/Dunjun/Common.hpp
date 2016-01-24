d1ea19669c0d37a343b1ad7a5a99191d53f7618c
Brian <heynairb@gmail.com>
2016-01-24 08:52:25 -0800

    first commit

---
+++ include/Dunjun/Common.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Brian Yeh on 1/24/16.
+//
+
+#ifndef DUNJUN_COMMON_HPP
+#define DUNJUN_COMMON_HPP
+
+namespace Dunjun
+{
+    using Float32 = float;
+    using Float64 = double;
+}
+
+#endif //DUNJUN_COMMON_HPP
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <Dunjun/Common.hpp>
+
+int main(int argc, char ** argv) {
+    std::cout << "Hello, World!\n";
+    std::cin.ignore();
+    return 0;
+}
