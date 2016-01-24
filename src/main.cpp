138bfe1c78ac46a7f23a82849efc39eec9979680
Brian <heynairb@gmail.com>
2016-01-24 14:21:13 -0800

    got it to compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
 #include <Dunjun/Common.hpp>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char ** argv) {
-    std::cout << "Hello, World!\n";
-    std::cin.ignore();
-    return 0;
+    GLFWwindow* window;
+
+    /* Initialize the Library */
+
+    if(!glfwInit()){
+        return EXIT_FAILURE;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(854, 480, "Dunjun", nullptr, nullptr);
+    if(!window){
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while(!glfwWindowShouldClose(window)){
+        /* Render here */
+
+        /*Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /*poll for and process events*/
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
